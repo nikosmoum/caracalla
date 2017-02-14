f8d192fa92b9de0ec63f7d290e98e656772b3279
Vritant Jain <adarshvritant@gmail.com>
2017-02-14 02:11:32 -0500

    Ensure stable test runs

--- ansible/resources/import.sql
+++ ansible/resources/import.sql
@@ -1,1 +1,13 @@
 ALTER DATABASE candlepin CHARACTER SET utf8 COLLATE utf8_general_ci;
+delete from cp_job;
+update QRTZ_TRIGGERS set next_fire_time = (select unix_timestamp(date_add(now(), interval 1 year)) * 1000);
+SET FOREIGN_KEY_CHECKS=0;
+delete from QRTZ_BLOB_TRIGGERS;
+delete from QRTZ_CRON_TRIGGERS;
+delete from QRTZ_FIRED_TRIGGERS;
+delete from QRTZ_PAUSED_TRIGGER_GRPS;
+delete from QRTZ_SIMPLE_TRIGGERS;
+delete from QRTZ_SIMPROP_TRIGGERS;
+delete from QRTZ_TRIGGERS;
+delete from QRTZ_JOB_DETAILS;
+SET FOREIGN_KEY_CHECKS=1;
