db5cb9cd848ccde8717e5a157baefc05161d6254
Vritant Jain <adarshvritant@gmail.com>
2017-09-13 11:21:07 -0400

    Automatic commit using alias

--- parse-jtl.py
+++ parse-jtl.py
@@ -129,7 +129,7 @@ def compare_csv(input_dict, baseline_dict, deviance_dict):
         succ_rate = compare_success_rates(
                         key,
                         values['success_%'],
-                        deviance_dict[key]['required_success']
+                        baseline_dict[key]['success_%']
                     )
         if succ_rate != "":
             if Colors.NO_COLOR is False:
