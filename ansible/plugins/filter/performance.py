4621fdf7d4a156f67cf1d8b1b9dfb9ed8098ebd4
Jiri Hnidek <jhnidek@redhat.com>
2017-11-06 21:05:53 +0100

    Maintenance job for performance tests
    Changes to performance tests
    * Run param_estimator.py and for baseline.json and expected.json
    * Copy sucessful results to NFS share at host
    
    New maintenace job for perf tests
    * This job should be started manually
    * New version of baseline.json and expected.json is computed
      from all sucessful results
    * Commit changes to feature branch
    * Create pull request from feature branch

--- ansible/plugins/filter/performance.py
+++ ansible/plugins/filter/performance.py
@@ -22,9 +22,15 @@ def expected_file(baseline_branch):
     return 'expected/%s.%s-expected.json' % (version[0], version[1])
 
 
+def major_minor_num(baseline_branch):
+    version = baseline_branch.split('.')
+    return '%s.%s' % (version[0], version[1])
+
+
 class FilterModule(object):
     @staticmethod
     def filters():
         return {'snap_name': snap_name,
                 'baseline_file': baseline_file,
-                'expected_file': expected_file}
+                'expected_file': expected_file,
+                'major_minor_num': major_minor_num}
